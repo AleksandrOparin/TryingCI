f701998a920b816fa4483651db0804d78b0f265e
Aleksandr <sasha.oparin.1984@mail.ru>
2022-10-23 19:54:28 +0300

    Some changes

--- linters/check_before.sh
+++ linters/check_before.sh
@@ -9,7 +9,7 @@ print_header "RUN clang-tidy"
 check_log "clang-tidy project/*.cpp project/*.h -checks=-*,clang-analyzer-*,-clang-analyzer-cplusplus* -warnings-as-errors=* -- -DDEBUG -I ./include" "Error (?:reading|while processing)"
 
 print_header "RUN cpplint"
-check_log "cpplint project/*.cpp project/*.h --extensions=c++,cpp,hpp" "Can't open for reading"
+check_log "cpplint project/*.cpp project/*.h --extensions=c++,cc,cpp,h,hh,hpp" "Can't open for reading"
 
 print_header "RUN cppcheck"
 check_log "cppcheck project/*.cpp project/*.h -q -j4 --enable=performance,portability,warning --error-exitcode=1" "\(information\)"
